e0fea3e0110105d49922509236e0c30cf47cbf70
kaustubh-ronge <166821543+kaustubh-ronge@users.noreply.github.com>
2024-06-19 21:25:21 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let nav = document.querySelector('.navbar');
+window.onscroll =  function (){
+    if(document.documentElement.scrollTop > 100){
+        nav.classList.add('header-scrolled')
+    }
+    else{
+        nav.classList.remove('header-scrolled')
+    }
+}
+
+let navBar = document.querySelectorAll(".nav-link")
+let navCollapse = document.querySelector(".navbar-collapse.collapse")
+navBar.forEach(function(a){
+    a.addEventListener("click",()=>{
+        navCollapse.classList.remove("show")
+    })
+})
+
+
+   // This is optional if you want to ensure smooth transition for the card title
+document.querySelectorAll('.card-title').forEach(title => {
+    title.style.transition = 'transform 0.3s';
+  });
+  
+  // This part ensures the card title moves back down when mouse leaves the card
+  document.querySelectorAll('.card-content').forEach(cardContent => {
+    cardContent.addEventListener('mouseleave', () => {
+      cardContent.querySelector('.card-title').style.transform = 'translateY(0%)';
+    });
+  });
+  
