27d64e7c4fe1230fafea0669694b4bad8fedd7c5
kaustubh-ronge <166821543+kaustubh-ronge@users.noreply.github.com>
2024-06-20 13:41:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 let nav = document.querySelector('.navbar');
 window.onscroll =  function (){
-    if(document.documentElement.scrollTop > 100){
+    if(document.documentElement.scrollTop > 10){
         nav.classList.add('header-scrolled')
     }
     else{
         nav.classList.remove('header-scrolled')
     }
 }
+
+
+let screenWidth = window.innerWidth
+
+if(screenWidth >=320 && screenWidth<438){
+    nav.style ='background-color:#091020;'
+}
+
+
+
 
 let navBar = document.querySelectorAll(".nav-link")
 let navCollapse = document.querySelector(".navbar-collapse.collapse")
